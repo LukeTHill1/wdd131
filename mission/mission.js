9b354d5d45aa8112ae46b704bf27b68e7566bcb1
LukeTHill1 <hil22007@byui.edu>
2025-01-18 15:26:02 -0700

    prove 11

--- mission/mission.js
+++ mission/mission.js
@@ -1,1 +1,14 @@
-theme = document.getElementById("theme").addEventListener("change");
+const themeSelector = document.getElementById("theme");
+function changeTheme()
+{
+    const selectedTheme = themeSelector.value;
+    if (selectedTheme === 'dark') {
+        // Apply dark theme
+        body.classList.add('dark');
+        
+    } else {
+        // Apply light theme
+        body.classList.remove('dark');
+    }
+}
+themeSelector.addEventListener('change', changeTheme);
