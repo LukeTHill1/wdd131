ce4718ff4b3e2370c245f869df6deeb3ead091e4
LukeTHill1 <hil22007@byui.edu>
2025-01-22 12:42:55 -0700

    mission 2

--- mission/mission.js
+++ mission/mission.js
@@ -1,14 +1,18 @@
 const themeSelector = document.getElementById('themeSelector');
+const body = document.body;
+const logo = document.getElementById('logo');
 function changeTheme()
 {
     const selectedTheme = themeSelector.value;
     if (selectedTheme === 'dark') {
         // Apply dark theme
         body.classList.add('dark');
+        logo.src = 'byui-logo_white.png';
         
     } else {
         // Apply light theme
         body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.wedp';
     }
 }
 themeSelector.addEventListener('change', changeTheme);
