b9b9cd016d0344d2df4ac64e785a3cee38e2de92
LukeTHill1 <hil22007@byui.edu>
2025-03-17 12:33:49 -0600

    wedfgbnmm

---
+++ recipes/main.js
@@ -0,0 +1,14 @@
+import recipes from './recipes.mjs';
+
+function random(num) {
+	return Math.floor(Math.random() * num);
+}
+
+function getRandomListEntry(list) {
+	const listLength = list.length;
+	const randomNum = random(listLength);
+	return list[randomNum];
+}
+
+// to test
+console.log(getRandomListEntry(recipes));
