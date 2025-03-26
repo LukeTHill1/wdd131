a322672446d8acc03ae30fa80155d1224acb2e6a
LukeTHill1 <hil22007@byui.edu>
2025-03-26 13:30:16 -0600

    Changed the other javascripts to the function

--- final/java/intro.js
+++ final/java/intro.js
@@ -1,18 +1,12 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
-        this.classList.toggle("active");
+function initalizeAccordion(className) {
+    var acc = document.getElementsByClassName(className);
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
 
-            /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-        panel.style.display = "none";
-        } 
-        else {
-        panel.style.display = "block";
-        }
-    });
+            var panel = this.nextElementSibling;
+            panel.style.display = (panel.style.display === "block") ? "none" : "block";
+        });
+    }
 }
+initalizeAccordion("accordion");
